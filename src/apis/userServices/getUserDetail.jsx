1e891a45b77ccb8abf67c5f8b00f637a640d7531
duong2607 <duongtungnguyen99@gmail.com>
2025-05-03 11:07:50 +0700

    fix func getUserDetail

--- src/apis/userServices/getUserDetail.jsx
+++ src/apis/userServices/getUserDetail.jsx
@@ -4,7 +4,10 @@ const getUserDetail = async () => {
     try {
         console.log('call api get user detail');
         const apiGetUserDetail = `/api/v1/users/${localStorage.getItem('userId')}`;
-        const response = await get(apiGetUserDetail);
+        const response = await get(
+            apiGetUserDetail,
+            localStorage.getItem('token')
+        );
         console.log('call api get user detail completely');
         return response.data;
     } catch (error) {
